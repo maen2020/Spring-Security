48ff729eec88eb6fdfcc0f53b670d3f89c4bc2ca
maen <maen210212@gmail.com>
2023-06-27 14:33:34 -0600

    Adding basic spring configuration, second method.

--- src/main/java/com/maen/config/SecurityConfig.java
+++ src/main/java/com/maen/config/SecurityConfig.java
@@ -10,17 +10,31 @@ import org.springframework.security.web.SecurityFilterChain;
 @EnableWebSecurity
 public class SecurityConfig {
 
+    /*
     // Configuracion numero 1 con authorizeHttpRequest casi deprecado.
     @Bean //Sirve para indicar que este bean será administrado por Spring Boot
     public SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception {
         return httpSecurity
                 .authorizeHttpRequests() //Configurar cuales URL's van a estar permitidas o protegidas.
-                    .requestMatchers("/v1/index").permitAll() //Arreglo de string de urls sin autorizacion.
+                    .requestMatchers("/v1/index2").permitAll() //Arreglo de string de urls sin autorizacion.
                     .anyRequest().authenticated() //Cualquier otra url necesita autenticacion.
                 .and()
                 .formLogin().permitAll() //Permitir a todos el Login.
                 .and()
                 .httpBasic()
+                .and()
+                .build();
+    }*/
+
+    //Configuracion basica #2 con funcion lambda.
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception{
+        return httpSecurity
+                .authorizeHttpRequests( auth -> {
+                    auth.requestMatchers("/v1/index2").permitAll();
+                    auth.anyRequest().authenticated();
+                })
+                .formLogin().permitAll()
                 .and()
                 .build();
     }

52ab51f9b8f41e9a7f15e070b2845780e522a0b7
maen <maen210212@gmail.com>
2023-06-27 14:53:05 -0600

    Adding basic configuration with spring redirection, third method.

--- src/main/java/com/maen/config/SecurityConfig.java
+++ src/main/java/com/maen/config/SecurityConfig.java
@@ -4,7 +4,11 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.session.SessionRegistry;
+import org.springframework.security.core.session.SessionRegistryImpl;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
 
 @Configuration
 @EnableWebSecurity
@@ -26,7 +30,11 @@ public class SecurityConfig {
                 .build();
     }*/
 
-    //Configuracion basica #2 con funcion lambda.
+    /*
+    /**
+     * Configuracion basica #2 con funcion lambda.
+     */
+    /*
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception{
         return httpSecurity
@@ -37,5 +45,31 @@ public class SecurityConfig {
                 .formLogin().permitAll()
                 .and()
                 .build();
+    }*/
+
+    /**
+     * Configuraion #3 un poco mas compleja al asegurar una aplicacion o endpoints.
+     */
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception {
+        return httpSecurity
+                .authorizeHttpRequests( auth -> {
+                    auth.requestMatchers("/v1/index2").permitAll();
+                    auth.anyRequest().authenticated();
+                })
+                .formLogin()
+                    .successHandler(successHandler()) //Url a donde se va a dirigir despues de iniciar sesion.
+                    .permitAll()
+                .and()
+                .build();
+    }
+
+    /**
+     * Metodo que redirecciona a un endpoint especifico al ingresar las credenciales correctamente.
+     */
+    public AuthenticationSuccessHandler successHandler(){
+        return ((request, response, authentication) -> {
+            response.sendRedirect("/v1/index");
+        });
     }
 }
